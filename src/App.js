1860f7b0f16547687dc8a8838108f2856313b9ab
Gerrit Schulz-Telschow <gerrit.schulz.telschow@gmail.com>
2022-04-22 13:18:59 +0200

    deleted upload route

--- src/App.js
+++ src/App.js
@@ -67,7 +67,6 @@ export default function App() {
             />
           }
         />
-        <Route path="/upload" element={<UploadTest />} />
       </Routes>
       <Navigation />
     </AppContainer>
